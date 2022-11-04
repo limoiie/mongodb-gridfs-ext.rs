f5d4022996f438f69f00073bbb0c4f42af077940
limoiie <limo.iie4@gmail.com>
2022-11-04 22:10:23 +0800

    test: Test cases for common extended methods of GridFSBucket

--- src/bucket/common.rs
+++ src/bucket/common.rs
@@ -46,3 +46,130 @@ impl GridFSBucketExt for GridFSBucket {
         Ok(cursor.next().await.is_some())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::error::GridFSError::FileNotFound;
+    use crate::error::GridFSExtError::GridFSError;
+    use fake::Fake;
+    use mongodb::Client;
+    use test_utilities::docker;
+    use test_utilities::gridfs::{TempFile, TempFileFaker};
+
+    #[tokio::test]
+    async fn test_id() {
+        let filename = "some-filename.txt";
+
+        let handle = docker::Builder::new("mongo")
+            .port_mapping(0, Some(27017))
+            .build_disposable()
+            .await;
+
+        let bucket = GridFSBucket::new(
+            Client::with_uri_str(handle.url.as_ref().unwrap())
+                .await
+                .unwrap()
+                .database("test_db"),
+            None,
+        );
+
+        let file = TempFileFaker::with_bucket(bucket.clone())
+            .name(filename.into())
+            .fake::<TempFile>();
+
+        assert_eq!(file.id, bucket.id(filename).await.unwrap());
+        match bucket.id("non-exist-filename.txt").await.unwrap_err() {
+            GridFSError(FileNotFound()) => (),
+            _ => assert!(false, "Should return error [GridFSError(FileNotFound())]"),
+        }
+    }
+
+    #[tokio::test]
+    async fn test_read_as_string() {
+        let filename = "some-filename.txt";
+
+        let handle = docker::Builder::new("mongo")
+            .port_mapping(0, Some(27017))
+            .build_disposable()
+            .await;
+
+        let bucket = GridFSBucket::new(
+            Client::with_uri_str(handle.url.as_ref().unwrap())
+                .await
+                .unwrap()
+                .database("test_db"),
+            None,
+        );
+
+        let file = TempFileFaker::with_bucket(bucket.clone())
+            .name(filename.into())
+            .include_content(true)
+            .fake::<TempFile>();
+
+        assert_eq!(
+            file.content.unwrap().as_slice(),
+            bucket
+                .read_as_string(bucket.id(filename).await.unwrap())
+                .await
+                .unwrap()
+                .as_bytes()
+        );
+    }
+
+    #[tokio::test]
+    async fn test_read_as_bytes() {
+        let filename = "some-filename.txt";
+
+        let handle = docker::Builder::new("mongo")
+            .port_mapping(0, Some(27017))
+            .build_disposable()
+            .await;
+
+        let bucket = GridFSBucket::new(
+            Client::with_uri_str(handle.url.as_ref().unwrap())
+                .await
+                .unwrap()
+                .database("test_db"),
+            None,
+        );
+
+        let file = TempFileFaker::with_bucket(bucket.clone())
+            .name(filename.into())
+            .include_content(true)
+            .fake::<TempFile>();
+
+        assert_eq!(
+            file.content.unwrap().as_slice(),
+            bucket
+                .read_as_bytes(bucket.id(filename).await.unwrap())
+                .await
+                .unwrap()
+        );
+    }
+
+    #[tokio::test]
+    async fn test_exists() {
+        let filename = "some-filename.txt";
+
+        let handle = docker::Builder::new("mongo")
+            .port_mapping(0, Some(27017))
+            .build_disposable()
+            .await;
+
+        let bucket = GridFSBucket::new(
+            Client::with_uri_str(handle.url.as_ref().unwrap())
+                .await
+                .unwrap()
+                .database("test_db"),
+            None,
+        );
+
+        let _file = TempFileFaker::with_bucket(bucket.clone())
+            .name(filename.into())
+            .fake::<TempFile>();
+
+        assert!(bucket.exists(filename).await.unwrap());
+        assert!(!bucket.exists("non-exist-filename.txt").await.unwrap());
+    }
+}
