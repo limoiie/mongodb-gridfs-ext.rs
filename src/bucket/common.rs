bdd906960afea0d12fd74b0d8ea8694259d86fa5
limoiie <limo.iie4@gmail.com>
2022-12-13 10:45:11 +0800

    refactor: Support read/write as string/bytes

--- src/bucket/common.rs
+++ src/bucket/common.rs
@@ -2,22 +2,31 @@ use async_trait::async_trait;
 use futures::StreamExt;
 use mongodb::bson::doc;
 use mongodb::bson::oid::ObjectId;
-use mongodb_gridfs::options::GridFSFindOptions;
+use mongodb_gridfs::options::{GridFSFindOptions, GridFSUploadOptions};
 use mongodb_gridfs::{GridFSBucket, GridFSError};
 
 use crate::error::Result;
 
-/// Extend common helper methods to [mongodb_gridfs::GridFSBucket].
+/// Extend common helper methods to [GridFSBucket].
 #[async_trait]
 pub trait GridFSBucketExt {
     /// Get doc id by `filename`.
     async fn id(&self, filename: &str) -> Result<ObjectId>;
 
-    /// Read cloud file by id as [alloc::String].
-    async fn read_as_string(&self, id: ObjectId) -> Result<String>;
+    /// Get doc filename by `id`.
+    async fn filename(&self, id: ObjectId) -> Result<String>;
 
-    /// Read cloud file by id as [alloc::Vec<u8>].
-    async fn read_as_bytes(&self, id: ObjectId) -> Result<Vec<u8>>;
+    /// Read cloud file by `id` as [alloc::String].
+    async fn read_string(&self, id: ObjectId) -> Result<String>;
+
+    /// Read cloud file by `id` as [alloc::Vec<u8>].
+    async fn read_bytes(&self, id: ObjectId) -> Result<Vec<u8>>;
+
+    /// Write [&str] into cloud file by `id`.
+    async fn write_string(&mut self, id: ObjectId, content: &str) -> Result<()>;
+
+    /// Write [&\[u8\]] into cloud file by `id`.
+    async fn write_bytes(&mut self, id: ObjectId, content: &[u8]) -> Result<()>;
 
     /// Return true if there is a file on the cloud with `filename`.
     async fn exists(&self, filename: &str) -> Result<bool>;
@@ -36,16 +45,39 @@ impl GridFSBucketExt for GridFSBucket {
             .map_err(Into::into)
     }
 
-    async fn read_as_string(&self, id: ObjectId) -> Result<String> {
-        self.read_as_bytes(id)
+    async fn filename(&self, id: ObjectId) -> Result<String> {
+        let opt = GridFSFindOptions::default();
+        self.find(doc! {"_id": id}, opt)
+            .await?
+            .next()
+            .await
+            .ok_or(GridFSError::FileNotFound())?
+            .map(|doc| doc.get_str("filename").unwrap().to_owned())
+            .map_err(Into::into)
+    }
+
+    async fn read_string(&self, id: ObjectId) -> Result<String> {
+        self.read_bytes(id)
             .await
             .and_then(|bytes| std::io::read_to_string(bytes.as_slice()).map_err(|err| err.into()))
     }
 
-    async fn read_as_bytes(&self, id: ObjectId) -> Result<Vec<u8>> {
+    async fn read_bytes(&self, id: ObjectId) -> Result<Vec<u8>> {
         let mut cursor = self.open_download_stream(id).await?;
         let buffer = cursor.next().await.ok_or(GridFSError::FileNotFound())?;
         Ok(buffer)
+    }
+
+    async fn write_string(&mut self, id: ObjectId, content: &str) -> Result<()> {
+        self.write_bytes(id, content.as_bytes()).await
+    }
+
+    async fn write_bytes(&mut self, id: ObjectId, content: &[u8]) -> Result<()> {
+        let filename = self.filename(id).await?;
+        let opt = GridFSUploadOptions::default();
+        self.upload_from_stream(filename.as_str(), content, Some(opt))
+            .await?;
+        Ok(())
     }
 
     async fn exists(&self, filename: &str) -> Result<bool> {
@@ -57,13 +89,15 @@ impl GridFSBucketExt for GridFSBucket {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::error::GridFSError::FileNotFound;
-    use crate::error::GridFSExtError::GridFSError;
     use fake::Fake;
     use mongodb::Client;
     use test_utilities::docker;
     use test_utilities::gridfs::{TempFile, TempFileFaker};
+
+    use crate::error::GridFSError::FileNotFound;
+    use crate::error::GridFSExtError::GridFSError;
+
+    use super::*;
 
     #[tokio::test]
     async fn test_id() {
@@ -94,7 +128,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_read_as_string() {
+    async fn test_read_string() {
         let filename = "some-filename.txt";
 
         let handle = docker::Builder::new("mongo")
@@ -118,7 +152,7 @@ mod tests {
         assert_eq!(
             file.content.unwrap().as_slice(),
             bucket
-                .read_as_string(bucket.id(filename).await.unwrap())
+                .read_string(bucket.id(filename).await.unwrap())
                 .await
                 .unwrap()
                 .as_bytes()
@@ -126,7 +160,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_read_as_bytes() {
+    async fn test_read_bytes() {
         let filename = "some-filename.txt";
 
         let handle = docker::Builder::new("mongo")
@@ -150,7 +184,7 @@ mod tests {
         assert_eq!(
             file.content.unwrap().as_slice(),
             bucket
-                .read_as_bytes(bucket.id(filename).await.unwrap())
+                .read_bytes(bucket.id(filename).await.unwrap())
                 .await
                 .unwrap()
         );
